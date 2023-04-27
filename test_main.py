62cbabf6f51e6cea8daf73a8ec9a8cb866efe334
Kellerberg <r.carter.hpl@gmail.com>
2023-04-27 20:39:39 +0300

    Selenoid connection established. Test added.

--- test_main.py
+++ test_main.py
@@ -4,6 +4,6 @@ from selene import be, have
 
 def test_search(setup_browser):
     browser.config.driver = setup_browser
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('cats').press_enter()
-    browser.element('[id="search"]').should(have.text('Cat'))
+    browser.open('https://www.europeana.eu/en')
+    browser.element('[data-qa="search box"]').should(be.visible).click().type('cats').press_enter()
+    browser.element('[data-qa="search page"]').should(have.text('cats'))
