4e00de67f6efeeb53fa8865f906f3634c60f7bfd
Kellerberg <r.carter.hpl@gmail.com>
2023-04-27 20:32:47 +0300

    Selenoid connection STILL NOT established

--- test_main.py
+++ test_main.py
@@ -2,8 +2,8 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_search(driver):
-    browser.config.driver = driver
+def test_search(setup_browser):
+    browser.config.driver = setup_browser
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('cats').press_enter()
     browser.element('[id="search"]').should(have.text('Cat'))
