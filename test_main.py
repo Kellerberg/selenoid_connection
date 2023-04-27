c3a365ea4be71371fcbefeae09a66df3ae7c95c0
Kellerberg <r.carter.hpl@gmail.com>
2023-04-27 21:14:58 +0300

    Browser setup updated

--- test_main.py
+++ test_main.py
@@ -1,9 +1,11 @@
-from selene.support.shared import browser
+import allure
 from selene import be, have
 
 
 def test_search(setup_browser):
-    browser.config.driver = setup_browser
-    browser.open('https://www.europeana.eu/en')
-    browser.element('[data-qa="search box"]').should(be.visible).click().type('cats').press_enter()
-    browser.element('[data-qa="search page"]').should(have.text('cats'))
+
+    browser = setup_browser
+    with allure.step('Testing'):
+        browser.open('https://www.europeana.eu/en')
+        browser.element('[data-qa="search box"]').should(be.visible).click().type('cats').press_enter()
+        browser.element('[data-qa="search page"]').should(have.text('cats'))
