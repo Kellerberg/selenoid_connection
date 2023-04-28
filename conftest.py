ee8199859f66906dfb86b2c3d81aa05655de2e94
Kellerberg <r.carter.hpl@gmail.com>
2023-04-28 21:38:55 +0300

    Browser setup updated.

--- conftest.py
+++ conftest.py
@@ -8,13 +8,13 @@ from dotenv import load_dotenv
 
 from utils import attach
 
-DEFAULT_BROWSER_VERSION = "100.0"
+DEFAULT_BROWSER_VERSION = "98.0"
 
 
 def pytest_addoption(parser):
     parser.addoption(
         '--browser_version',
-        default='100.0'
+        default='98.0'
     )
 
 
@@ -29,7 +29,7 @@ def setup_browser(request):
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
-        "browserName": "chrome",
+        "browserName": "firefox",
         "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
