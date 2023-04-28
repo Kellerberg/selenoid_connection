cefd0ba6bdd89589a6b928adfadd7753d1bf3dd8
Kellerberg <r.carter.hpl@gmail.com>
2023-04-28 21:00:41 +0300

    Search input updated.

--- test_main.py
+++ test_main.py
@@ -2,10 +2,18 @@ import allure
 from selene import be, have
 
 
+@allure.title('Testing search')
 def test_search(setup_browser):
 
     browser = setup_browser
-    with allure.step('Testing'):
+
+    search = 'dogs'
+
+    with allure.step('Open main page'):
         browser.open('https://www.europeana.eu/en')
-        browser.element('[data-qa="search box"]').should(be.visible).click().type('cats').press_enter()
-        browser.element('[data-qa="search page"]').should(have.text('cats'))
+    with allure.step('Opening search box'):
+        browser.element('[data-qa="show search button"]').should(be.visible).click()
+    with allure.step('Input search request'):
+        browser.element('[data-qa="search box"]').should(be.visible).type(search).press_enter()
+    with allure.step('Checking results'):
+        browser.element('[data-qa="search page"]').should(have.text(search))
